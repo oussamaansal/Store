d63d337599fc25e286ec03ac7252919125c073ae
oussamaansal <oussama.ansal@outlook.com>
2025-06-08 13:16:54 +0200

    second commit

--- store/models.py
+++ store/models.py
@@ -4,3 +4,13 @@ from django.contrib.auth.models import AbstractUser
 
 class User_one(AbstractUser):
     pass
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
--- store/views.py
+++ store/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, AllowAny
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get_permissions(self):
+        if self.request.method in ["POST", "PUT", "DELETE"]:
+            return [IsAdminUser()]
+        return [AllowAny()]
